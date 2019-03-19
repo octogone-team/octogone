6b47dc63480d17c8df2b82393df23f3ab61ad8c7
phanke18 <phanke18@b032-20.crio.dauphine.fr>
2019-03-19 10:30:15 +0100

    first commit

---
+++ src/main/java/fr/dauphine/sia/App.java
@@ -0,0 +1,13 @@
+package fr.dauphine.sia;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
