d7a06874f63befe393dc01e9380c5fe4aa6fdabb
Kevin Phan <kphan0104@gmail.com>
2019-04-14 17:43:08 +0200

    Upgrade dependencies

--- src/test/java/fr/dauphine/sia/MoviesTest.java
+++ src/test/java/fr/dauphine/sia/MoviesTest.java
@@ -1,6 +1,8 @@
 package fr.dauphine.sia;
-import static org.junit.Assert.assertEquals;
-import org.junit.Test;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MoviesTest {
 
